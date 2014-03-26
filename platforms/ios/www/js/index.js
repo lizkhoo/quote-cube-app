d2283a03d95c1e551284ec0d7f0b9ead8fe48faf
Liz Khoo <emkhoo@gmail.com>
2014-03-26 14:35:09 -0400

    new version of cube app

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,121 +1,232 @@
-
 var app = {
+/*
+	location for the messageDiv, in percentage:
+*/
+	loc: {
+		x: 10,
+		y: 10
+	},
+/*
+	Application constructor
+*/
+	initialize: function() {
+		this.bindEvents();			// bind any UI events to listeners
+		console.log("Starting Accelerometer app");
+	},
+/*
+	bind any events that are required on startup to listeners:
+*/
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener('click', this.resetScreen, false);
+	},
 
-//location for messageDiv, in percentage:
-    loc: {
-        x: 10,
-        y: 10
-    },
+/*
+	this runs when the device is ready for user interaction:
+*/
+    // generate a random number 1-10
 
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-        console.log("starting app");
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('click', this.resetScreen, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.watchAcceleration();
-        app.renderCube();
-    },
+    var quoteNum;
+    var quotes = new Array(
+        "Look for the question, not the solution.",
+        "Observe, imagine and create.",
+        "Creativity is not the game preserve of artists, but an intrinsic feature of all human activity.",
+        "In any creative endeavor you will be discomfited and that is part of learning.",
+        "Communicate emotion.",
+        "Don’t see the world as a market, but rather a place that people live 
+        in — you are designing for people, not machines.",
+        "Be willing to risk, make mistakes, and learn from failure."
+        );
 
-    watchAcceleration: function(){
-        function success(acceleration) {
-            var xpos = acceleration.x.toFixed(2);
-            var ypos = acceleration.y.toFixed(2);
-            var zpos = (acceleration.z - 9.80).toFixed(2);
+	onDeviceReady: function() {
+		// start watching the accelerometer:
+		app.watchAcceleration();
+	},
 
-            app.loc.x -= acceleration.x;
-            app.loc.y -= acceleration.y;
+	watchAcceleration: function() {
+		function success(acceleration) {
+			
+            quoteNum = Math.floor((Math.random()*7)+1); 
 
-            console.log('xpos: '+ xpos);
-            console.log('ypos: '+ ypos);
-            console.log('zpos: '+ zpos);
-        }
-
-        function failure(error) {
-            console.log('error');
-        }
-    },
-
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },
-
-    renderCube: function() {
-        var canvas = document.getElementById("myCanvas");
-            
-            var renderer = new Pre3d.Renderer(canvas);
-            
-            var cube = Pre3d.ShapeUtils.makeCube(1);
-            // Avoid anti-aliasing cracks along quad faces.  We convert to triangles so
-            // that overdraw with overlap the textures along the edges.
-            Pre3d.ShapeUtils.triangulate(cube);
-            
-            renderer.fill_rgba = new Pre3d.RGBA(1, 0, 0, 1);
-            
-            var state = {
-                cube_rotate_y_rad: 0.4,
-                cube_rotate_x_rad: 0,
-                cube_x: 0,
-                cube_y: 0
-            };
-            
-            function draw(){
-                initCanvasArea(canvas);
-
-                renderer.transform.reset();
-                renderer.transform.rotateX(state.cube_rotate_x_rad);
-                renderer.transform.rotateY(state.cube_rotate_y_rad);
-                //renderer.transform.translate(state.cube_x, state.cube_y, 0);
-                renderer.bufferShape(cube);
-                
-                // White background.
-                renderer.ctx.setFillColor(1, 1, 1, 1);
-                renderer.drawBackground();
-                
-                renderer.drawBuffer();
-                renderer.emptyBuffer();
-            };
-            
-            renderer.camera.focal_length = 2.5;
-            DemoUtils.autoCamera(renderer, 0, 0, -8, 0, 0, 0, draw);
-            
-            function initCanvasArea(cnv) {
-                cnv.width = window.innerWidth;
-                cnv.height = ((window.innerHeight)*0.8);
-            };
-            
-            draw();
-
-
-    }
-
+            // clear the messageDiv and add the accelerometer values:
+			app.clear();
+			app.display('X: ' + acceleration.x.toFixed(2));
+			app.display('Y: ' + acceleration.y.toFixed(2));
+			app.display('Z: ' + (acceleration.z - 9.80).toFixed(2));
+            app.display('quote ' + return quotes[quoteNum]; );
+			
+			// set app.loc using the accelerometer values:
+			app.loc.x -= acceleration.x;
+			app.loc.y -= acceleration.y;
+			
+			// set the messageDiv style parameters using app.loc:
+			messageDiv.style.top = app.loc.y  + '%';
+			messageDiv.style.left = app.loc.x + '%';
+		}
+			
+		function failure(error) {
+			// if the accelerometer fails, display the error:
+			app.display('Accelerometer error');
+			app.display(error);
+		}
+			
+		// taceh the accelerometer every 100ms:	
+		var watchAccel = navigator.accelerometer.watchAcceleration(success, failure, {
+			frequency: 100
+		});
+	},
+	
+	// reset the messageDiv to the center of the screen:
+	resetScreen: function() {
+		app.loc.x = 50;
+		app.loc.y = 50;
+		messageDiv.style.top = app.loc.y  + '%';
+		messageDiv.style.left = app.loc.x + '%';
+	},
+	/*
+		appends @message to the message div:
+	*/
+	display: function(message) {
+		var label = document.createTextNode(message),
+			lineBreak = document.createElement("br");
+		messageDiv.appendChild(lineBreak);			// add a line break
+		messageDiv.appendChild(label);				// add the text
+	},
+	/*
+		clears the message div:
+	*/
+	clear: function() {
+		messageDiv.innerHTML = "";
+	}
 };
 
-$( document ).on( "deviceready", function(){
-        StatusBar.overlaysWebView( false );
-        StatusBar.backgroundColorByName("gray");
-});
+$(function(){
 
+    var el = document.createElement('div'),
+        transformProps = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
+        transformProp = support(transformProps),
+        transitionDuration = 'transitionDuration WebkitTransitionDuration MozTransitionDuration OTransitionDuration msTransitionDuration'.split(' '),
+        transitionDurationProp = support(transitionDuration);
 
-            
+    function support(props) {
+        for(var i = 0, l = props.length; i < l; i++) {
+            if(typeof el.style[props[i]] !== "undefined") {
+                return props[i];
+            }
+        }
+    }
+
+    var mouse = {
+            start : {}
+        },
+        touch = document.ontouchmove !== undefined,
+        viewport = {
+            x: -10,
+            y: 20,
+            el: $('.cube')[0],
+            move: function(coords) {
+                if(coords) {
+                    if(typeof coords.x === "number") this.x = coords.x;
+                    if(typeof coords.y === "number") this.y = coords.y;
+                }
+
+                this.el.style[transformProp] = "rotateX("+this.x+"deg) rotateY("+this.y+"deg)";
+            },
+            reset: function() {
+                this.move({x: 0, y: 0});
+            }
+        };
+
+    viewport.duration = function() {
+        var d = touch ? 50 : 500;
+        viewport.el.style[transitionDurationProp] = d + "ms";
+        return d;
+    }();
+
+    $(document).keydown(function(evt) {
+        switch(evt.keyCode)
+        {
+            case 37: // left
+                viewport.move({y: viewport.y - 90});
+                break;
+
+            case 38: // up
+                evt.preventDefault();
+                viewport.move({x: viewport.x + 90});
+                break;
+
+            case 39: // right
+                viewport.move({y: viewport.y + 90});
+                break;
+
+            case 40: // down
+                evt.preventDefault();
+                viewport.move({x: viewport.x - 90});
+                break;
+
+            case 27: //esc
+                viewport.reset();
+                break;
+
+            default:
+                break;
+        };
+    }).bind('mousedown touchstart', function(evt) {
+        delete mouse.last;
+        if($(evt.target).is('a, iframe')) {
+            return true;
+        }
+
+        evt.originalEvent.touches ? evt = evt.originalEvent.touches[0] : null;
+        mouse.start.x = evt.pageX;
+        mouse.start.y = evt.pageY;
+        $(document).bind('mousemove touchmove', function(event) {
+            // Only perform rotation if one touch or mouse (e.g. still scale with pinch and zoom)
+            if(!touch || !(event.originalEvent && event.originalEvent.touches.length > 1)) {
+                event.preventDefault();
+                // Get touch co-ords
+                event.originalEvent.touches ? event = event.originalEvent.touches[0] : null;
+                $('.viewport').trigger('move-viewport', {x: event.pageX, y: event.pageY});
+            }
+        });
+
+        $(document).bind('mouseup touchend', function () {
+            $(document).unbind('mousemove touchmove');
+        });
+    });
+
+    $('.viewport').bind('move-viewport', function(evt, movedMouse) {
+
+        // Reduce movement on touch screens
+        var movementScaleFactor = touch ? 4 : 1;
+
+        if (!mouse.last) {
+            mouse.last = mouse.start;
+        } else {
+            if (forward(acceleration.x, mouse.last.x) != forward(mouse.last.x, movedMouse.x)) {
+                mouse.start.x = mouse.last.x;
+            }
+            if (forward(mouse.start.y, mouse.last.y) != forward(mouse.last.y, movedMouse.y)) {
+                mouse.start.y = mouse.last.y;
+            }
+        }
+
+        viewport.move({
+            x: viewport.x + parseInt((mouse.start.y - movedMouse.y)/movementScaleFactor),
+            y: viewport.y - parseInt((mouse.start.x - movedMouse.x)/movementScaleFactor)
+        });
+
+        mouse.last.x = movedMouse.x;
+        mouse.last.y = movedMouse.y;
+
+        function forward(v1, v2) {
+            return v1 >= v2 ? true : false;
+        }
+    });
+
+    /* Just for fun */
+    if(!touch) {
+        $('.cube > div').eq(2).html('<object width="360" height="360"><param name="movie" value="http://www.youtube.com/v/MY5PkidV1cM?fs=1&amp;hl=en_GB&amp;rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/MY5PkidV1cM?fs=1&amp;hl=en_GB&amp;rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="360" height="360"></embed></object>');
+    }
+
+});			// end of app
